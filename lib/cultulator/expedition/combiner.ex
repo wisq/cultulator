07d9a14eb7674257ce322a4f35cc6925919a9830
Adrian Irving-Beer <wisq@wisq.net>
2018-12-27 00:33:35 -0500

    More complete combiner.  Max combined solution length is 3.

--- lib/cultulator/expedition/combiner.ex
+++ lib/cultulator/expedition/combiner.ex
@@ -17,14 +17,36 @@ defmodule Cultulator.Expedition.Combiner do
   def combine(combos) do
     combos
     |> Enum.map(&sort_combo_fields/1)
-    |> Enum.group_by(&remove_no_curse/1)
-    |> Enum.map(fn {template, cs} ->
-      option_lists =
-        Enum.map(cs, & &1.for_no_curse)
-        |> zip_options()
+    |> combine_no_curse()
+    |> Enum.group_by(& &1.for_no_curse)
+    |> Enum.flat_map(fn {_fnc, cs} -> combine_success(cs) end)
+  end
 
-      %Combo{template | for_no_curse: option_lists}
+  defp combine_no_curse(combos) do
+    combos
+    |> Enum.group_by(&remove_no_curse/1)
+    |> Enum.flat_map(fn {template, cs} ->
+      Enum.map(cs, & &1.for_no_curse)
+      |> merge_options()
+      |> Enum.map(fn opts ->
+        %Combo{template | for_no_curse: opts}
+      end)
     end)
+  end
+
+  defp combine_success(combos) do
+    template = List.first(combos)
+
+    combos
+    |> Enum.map(& &1.for_success)
+    |> merge_options()
+    |> Enum.map(fn opts ->
+      %Combo{template | for_success: opts}
+    end)
+  end
+
+  defp count_in_combos(item, lists) do
+    Enum.count(lists, &(item in &1))
   end
 
   defp sort_combo_fields(%Combo{for_success: fs, for_no_curse: fnc} = c) do
@@ -33,13 +55,49 @@ defmodule Cultulator.Expedition.Combiner do
 
   defp remove_no_curse(%Combo{} = c), do: %Combo{c | for_no_curse: nil}
 
-  def zip_options([[]]), do: []
+  def merge_options(lists) do
+    cond do
+      # Just one list; return it.
+      length(lists) == 1 ->
+        lists
 
-  def zip_options(combos) do
-    firsts = Enum.map(combos, &List.first/1) |> Enum.uniq()
-    rests = Enum.map(combos, &Enum.drop(&1, 1)) |> Enum.uniq()
-    options = %Choice{list: firsts}
+      # All lists are length 1.
+      Enum.all?(lists, &(length(&1) == 1)) ->
+        # Combine them into a single choice:
+        options =
+          lists
+          |> List.flatten()
+          |> Enum.uniq()
 
-    [options | zip_options(rests)]
+        [[%Choice{list: options}]]
+
+      # Lists are longer than 1.
+      true ->
+        # Find the most popular elements:
+        most_popular =
+          List.flatten(lists)
+          |> Enum.uniq()
+          |> Enum.sort_by(&{-count_in_combos(&1, lists), &1})
+
+        # Going from most to least popular,
+        lists
+        # ... find lists that match <item>,
+        |> Enum.group_by(&earliest_match(&1, most_popular))
+        # ... and make a new set of lists that are
+        #     <item> + zipped sub-choices.
+        |> Enum.flat_map(fn {index, matching} ->
+          first = Enum.at(most_popular, index)
+
+          matching
+          |> Enum.map(&List.delete(&1, first))
+          |> merge_options()
+          |> Enum.map(fn zipped -> [first | zipped] end)
+        end)
+    end
+  end
+
+  defp earliest_match(list, popular) do
+    popular
+    |> Enum.find_index(&Enum.member?(list, &1))
   end
 end
